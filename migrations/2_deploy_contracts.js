ca356f19fc74dc2a84dbeae628a89a03f537ecba
Ubuntu20 <vishnuc77@gmail.com>
2021-08-14 20:53:00 +0530

    added dotenv to safely store mnemonic and added deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var MyTokenEth = artifacts.require("./MyTokenEth.sol");
+var MyTokenBsc = artifacts.require("./MyTokenBsc.sol");
+var ERCBridgeEth = artifacts.require("./ERCBridgeEth.sol");
+var ERCBridgeBsc = artifacts.require("./ERCBridgeBsc.sol");
+require("dotenv").config({ path: "../.env" });
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
+  let addr = await web3.eth.getAccounts();
+  await deployer.deploy(MyTokenEth);
+  let tokenEthInstance = await MyTokenEth.deployed();
+  await tokenEthInstance.mint(addr[0], process.env.INITIAL_TOKENS);
+  await deployer.deploy(ERCBridgeEth, tokenEthInstance.address);
+  let bridgeEthInstance = await ERCBridgeEth.deployed();
+  tokenEthInstance.changeOwner(bridgeEthInstance.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({ path: './.env' });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,17 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby_infura: {
+      provider: function () {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/v3/e231165cabd748bab6b079a9a57c530c", MetaMaskAccountIndex)
+      },
+      network_id: 4
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0"
     }
   }
 };
